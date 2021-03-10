76adf11fe3c82d5199c018fd4905858f971cddbc
Alexis Arando <lex4736@gmail.com>
2021-03-09 20:56:29 -0800

    Added custom layout, css and navigation for the application

--- client/src/App.js
+++ client/src/App.js
@@ -5,49 +5,64 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Login from "./Pages/Loginpage";
 import Signup from "./Pages/Signup";
+import Mmain from "./Pages/Mmain";
+import Emain from "./Pages/Emain";
+import Schedule from "./Pages/Schedulepage";
+import Profile from "./Pages/Profilepage";
+import Pg03 from "./Pages/Page03";
+import Pg04 from "./Pages/Page04";
 import UserContext from "./Context/UserContext"
 
+
+
+
 function App() {
-	const [userData, setUserData] = useState({
-		user: undefined,
-		token: undefined,
-	});
+	{/* ==============HIDE CODE 21 - 55 to see main page/UNHIDE to test authentication================ */}
+	// const [userData, setUserData] = useState({
+	// 	user: undefined,
+	// 	token: undefined,
+	// });
 
-	const checkLoggedIn = async () => {
-		let token = localStorage.getItem("auth-token");
+	// const checkLoggedIn = async () => {
+	// 	let token = localStorage.getItem("auth-token");
 
-		if (token === null) {
-			localStorage.setItem("auth-token", "");
-			token = "";
-		} else {
-			const userRes = await axios.get("/", {
-				headers: { "x-auth-token": token },
-			});
+	// 	if (token === null) {
+	// 		localStorage.setItem("auth-token", "");
+	// 		token = "";
+	// 	} else {
+	// 		const userRes = await axios.get("/", {
+	// 			headers: { "x-auth-token": token },
+	// 		});
 
-			setUserData({
-				token,
-				user: userRes.data,
-			});
-		}
-	};
+	// 		setUserData({
+	// 			token,
+	// 			user: userRes.data,
+	// 		});
+	// 	}
+	// };
 
-	const logout = () => {
-		setUserData({
-			token: undefined,
-			user: undefined,
-		});
+	// const logout = () => {
+	// 	setUserData({
+	// 		token: undefined,
+	// 		user: undefined,
+	// 	});
 
-		localStorage.setItem("auth-token", "");
-	};
+	// 	localStorage.setItem("auth-token", "");
+	// };
 
-	useEffect(() => {
-		checkLoggedIn();
-	}, []);
+	// useEffect(() => {
+	// 	checkLoggedIn();
+	// }, []);
+{/* ========================================= */}
 
 	return (
 		<div className="App">
-			<BrowserRouter>
-				{!userData.user ? (
+			
+			 <BrowserRouter>
+
+{/* ============HIDE CODE 64 - 76 ================== */}
+
+				 {/* {!userData.user ? (
 					<>
 						<Link to="/signup">Signup</Link>
 						<Link to="/login">Login</Link>
@@ -56,17 +71,35 @@ function App() {
 					<Link to="/login" onClick={logout}>
 						Logout
 					</Link>
-				)}
+				)} 
 
-				<UserContext.Provider value={{ userData, setUserData }}>
+				<UserContext.Provider value={{ userData, setUserData }}> */}
+{/* ========================================= */}
+
 					<Switch>
+{/* ===========HIDE CODE 81 - 84============ */}
+{/* 
 						<Route path="/login" component={Login} />
 						<Route path="/signup" component={Signup} />
-						<Route path="/" component={Homepage} />
+						<Route path="/" component={Homepage} /> */}
+{/* ========================================= */}
+
+						<Route path="/Mmain" component={Mmain}/>
+						<Route path="/Emain" component={Emain}/>
+                        <Route path="/Schedulepage" component={Schedule}/>
+                        <Route path="/Profilepage" component={Profile}/>
+						<Route path="/Page03" component={Pg03}/>
+						<Route path="/Page04" component={Pg04}/>
+
+
 					</Switch>
-				</UserContext.Provider>
+	{/* =============HIDE CODE 97============= */}				
+				{/* </UserContext.Provider> */}
 			</BrowserRouter>
 		</div>
+
 	);
 }
+
+
 export default App;
