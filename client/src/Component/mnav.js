006cc5f95acf5f44745255bd8407ab119c3df982
Alexis Arando <lex4736@gmail.com>
2021-03-16 20:29:28 -0700

    Fixed links to nav bar and moved heading to a component

--- client/src/Component/mnav.js
+++ client/src/Component/mnav.js
@@ -1,32 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import icon from "../Img/774.png";
+
 
 
 function Mnav() {
 	return (
 		<header>
-			
-			<span class="top">EMPLOYEE MANAGEMENT PORTAL</span>{" "}
-			<span class="coname"> Company Name</span>
-			<img src={icon} alt="icon"></img>
 			<nav class="menubar">
 				<ul>
 					<li>
 						<Link to="/Mmain"> HOME</Link>
 					</li>
 					<li>
-						<Link to="/Schedulepage">SCHEDULE</Link>
+						<Link to="/Mschedule">SCHEDULE</Link>
 					</li>
 					<li>
 						<Link to="/Profilepage">EMPLOYEE PROFILES</Link>
 					</li>
+		
 					<li>
-						<Link to="/Page03">CLOCK IN/OUT</Link>
+						<Link to="/mannoucements">ANNOUCEMENTS</Link>
 					</li>
-					<li>
-						<Link to="/Page04">ANNOUCEMENTS</Link>
-					</li>
+
 					<li><Link to="/Loginpage">LOGOUT</Link></li>
 				</ul>
 				<br></br>
