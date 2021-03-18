60629331e4748d1eb1bf10b5953e33c22631bb22
Victor Tran <victortran415@gmail.com>
2021-03-18 14:29:16 -0700

    ADD: installed npm moment

--- client/src/Component/AnnounceContainer.js
+++ client/src/Component/AnnounceContainer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DeleteBtn from "./DeleteBtn/index";
 
+
 const AnnounceContainer = () => {
 	const [form, setForm] = useState({ title: "", text: "" });
 
@@ -26,7 +27,6 @@ const AnnounceContainer = () => {
 		}
 		window.location.reload();
 	};
-
 	const deletePost = async (id) => {
 		try {
 			await axios.delete(`/announce/${id}`, {
@@ -43,23 +43,6 @@ const AnnounceContainer = () => {
 			const allPosts = await axios.get("/announce", {
 				headers: { "x-auth-token": localStorage.getItem("auth-token") },
 			});
-
-			//NOTE: This changes the format of the date, need to implement this to createdAt
-			const setTime = () => {
-				let lastAddedTime = allPosts.data.length - 1;
-				if (lastAddedTime >= 0) {
-					let newTime = new Date(
-						allPosts.data[lastAddedTime].createdAt
-					).toLocaleDateString("en-US", {
-						month: "short",
-						day: "numeric",
-						hour: "2-digit",
-						minute: "2-digit",
-						timeZone: "America/Los_Angeles",
-					});
-					console.log(newTime);
-				}
-			};
 
 			setPosts(allPosts.data.reverse());
 		})();
@@ -97,7 +80,8 @@ const AnnounceContainer = () => {
 				<tbody>
 					{posts.map((post, index) => (
 						<tr key={index}>
-							<th scope="row">{post.createdAt}</th>
+							<th scope="row">
+							</th>
 							<td>{post.title}</td>
 							<td>{post.text}</td>
 							<td>
