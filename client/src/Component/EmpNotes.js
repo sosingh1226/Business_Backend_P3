675498687ac24a76261558e69c8a2c7b5b17c5ca
soumyas <so.singh1226@gmail.com>
2021-03-22 22:33:07 -0400

    Employee Profile Page

---
+++ client/src/Component/EmpNotes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Mnav from "../Component/mnav";
+import Heading from "../Component/heading";
+import AnnounceContainer from "../Component/AnnounceContainer";
+
+const EmpNotes = () => {
+	return (
+		<div class="pagetitle">
+			<h2>ANNOUNCEMENTS</h2>
+			Notes and Reminders
+			<br></br>
+			<AnnounceContainer />
+		</div>
+	);
+};
+
+export default EmpNotes;
---
+++ client/src/utils/emputils.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+export default class Emputils extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        classBtnVal: { name: 'none', email: 'none', cell: 'none', id: 'none', username: 'none', password: 'none', dob: 'none' },
+        searchName: '',
+        employees: this.props.employees.results
+    };
+
+    render() {
+        const { classBtnVal, searchName, employees } = this.state;
+        // const { data } = this.props;
+
+        return (
+            <div className="container02">
+                {/* <table className="table table-primary table-striped"> */}
+                <table className="table3">
+                    <thead className="thead2">
+                        <tr>
+                            <th scope="col">PICTURE</th>
+                            <th scope="col">EMAIL</th>
+                            <th scope="col">PHONE NUMBER</th>
+                            <th scope="col">ID REF</th>
+                            <th scope="col">USERNAME</th>
+                            <th scope="col">PASSWORD</th>
+                            <th scope="col">DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map((employee) => (
+                            <tr className="table-row" key={employee.login.uuid}>
+                                <td>
+                                    <img src={employee.picture.large} alt="employee photo" />
+                                </td>
+                                <td>{employee.email}</td>
+                                <td>{employee.cell}</td>
+                                <td>{employee.id.name} {employee.id.value}</td>
+                                <td>{employee.login.username}</td>
+                                <td>{employee.login.password}</td>
+                                <td>{employee.dob.date}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
