3d88fa8fdc0a1325e1f9544cd3ce2f1f4dace4e3
Victor Tran <victortran415@gmail.com>
2021-03-25 13:59:32 -0700

    UPDATE: link confirm controller with routes

--- routes/confirmRoutes.js
+++ routes/confirmRoutes.js
@@ -1,7 +1,6 @@
-const router = require("express").Router()
-const {userConfirm} = require('../controllers/confirmController')
+const router = require("express").Router();
+const { userConfirm } = require("../controllers/confirmController");
 
+router.post("/", userConfirm);
 
-router.post("/", userConfirm)
-
-module.exports = router
+module.exports = router;
