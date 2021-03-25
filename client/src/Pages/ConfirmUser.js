fe9db2f0e8f29e06478fa3878afb957747a3f230
Victor Tran <victortran415@gmail.com>
2021-03-25 15:03:16 -0700

    UPDATE: ConfirmUser.js

--- client/src/Pages/ConfirmUser.js
+++ client/src/Pages/ConfirmUser.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-const ConfirmUser = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const ConfirmUser = (props) => {
+	const history = useHistory();
 
-export default ConfirmUser
+	useEffect(() => {
+		(async () => {
+			try {
+				await axios.get("/register", {
+					token: props.match.params.token,
+				});
+				history.push("/");
+			} catch (err) {
+				console.log(err);
+			}
+		})();
+	}, [history, props.match.params.token]);
+
+	return <div>User has been confirmed {props.match.params.token}</div>;
+};
+
+export default ConfirmUser;
