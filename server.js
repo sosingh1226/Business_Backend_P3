1853484db62026cd0f27b915d875a2103b0953f7
Victor Tran <victortran415@gmail.com>
2021-03-25 11:00:15 -0700

    DELETE: remove heading component because it was not being used

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
-require("dotenv").config();
+// require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
